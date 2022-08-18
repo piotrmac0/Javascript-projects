86355b53631aa3953c1336fe4689f1133791ffa1
Piotr Mac <104590583+piotrmac0@users.noreply.github.com>
2022-08-18 19:05:06 +0200

    Create Cookie_Popup.js

---
+++ Cookie_Popup/Cookie_Popup.js
@@ -0,0 +1,14 @@
+const cookiePopup = document.querySelector(".cookiePopup");
+const btnCookie = document.querySelector("acceptCookie");
+ 
+if(btnCookie) {
+btnCookie.addEventListener("click", () => {
+  cookiePopup.classList.add("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+}
+
+setTimeout(() => {
+  if(!localStorage.getItem("cookieBannerDisplayed"))
+  cookiePopup.classList.remove("active");
+}, 2000);
